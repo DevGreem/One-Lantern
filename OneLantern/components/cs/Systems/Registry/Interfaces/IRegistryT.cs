0a0a0dacf49a969366add9b19eed41712e9ea4f0
DevGreem <ian.pichardo@hotmail.com>
2026-09-07 03:19:53 -0400

    fix: Fixed old type name of RecordId in IRegistry<T>

--- OneLantern/components/cs/Systems/Registry/Interfaces/IRegistryT.cs
+++ OneLantern/components/cs/Systems/Registry/Interfaces/IRegistryT.cs
@@ -8,13 +8,13 @@ public partial interface IRegistry<T> : IRegistry
 
 	public bool Register(string id, T value);
 
-	public bool Register(RegistryId id, T value) => Register(id.ToString(), value);
+	public bool Register(RecordId id, T value) => Register(id.ToString(), value);
 
 	public bool TryGet(string id, out T? value);
 
-	public bool TryGet(RegistryId id, out T? value) => TryGet(id.ToString(), out value);
+	public bool TryGet(RecordId id, out T? value) => TryGet(id.ToString(), out value);
 
 	public T Get(string id);
 
-	public T Get(RegistryId id) => Get(id.ToString());
+	public T Get(RecordId id) => Get(id.ToString());
 } 
