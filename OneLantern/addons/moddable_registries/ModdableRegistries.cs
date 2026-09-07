68463b20ee01f64b46fa6710b545417bb693ff00
DevGreem <ian.pichardo@hotmail.com>
2026-09-07 16:08:53 -0400

    docs: Changed name of the plugin RegistryConfiguration to ModdableRegistries

--- OneLantern/addons/moddable_registries/ModdableRegistries.cs
+++ OneLantern/addons/moddable_registries/ModdableRegistries.cs
@@ -4,7 +4,7 @@ using Godot.Collections;
 using System;
 
 [Tool]
-public partial class RegistryConfiguration : EditorPlugin
+public partial class ModdableRegistries : EditorPlugin
 {
 
 	public const string GROUP = "RegistryConfiguration";
